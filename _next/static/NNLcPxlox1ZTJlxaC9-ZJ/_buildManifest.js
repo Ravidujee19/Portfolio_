ac417b938d27f844daf5a1b05ebe846be61ab07f
Ravidujee19 <Ravidujee19@users.noreply.github.com>
2025-12-13 06:48:33 +0000

    deploy: 02f1a43e813ff4e0615db7c858e7802cb3de4fe7

---
+++ _next/static/NNLcPxlox1ZTJlxaC9-ZJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b8770c9caa6ad0d6.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
